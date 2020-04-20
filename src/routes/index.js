12c8717bc753dde2e2b9c745a7f341251eb1bc55
Søren Spangsberg Jørgensen <smsj@easv.dk>
2020-04-20 13:34:44 +0200

    Environment variables setup and tested

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
-});
-module.exports = router;
-=======
 import express from 'express';
 import { APP_NAME } from '../settings';
 
@@ -14,4 +6,3 @@ const indexRouter = express.Router();
 indexRouter.get('/', (req, res) => res.status(200).json({ message: APP_NAME }));
 
 export default indexRouter;
->>>>>>> Stashed changes
